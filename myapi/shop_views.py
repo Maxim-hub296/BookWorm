532b615078be23b5291b26a396bbd9d2378f6d88
Maxim <maxsavikin@gmail.com>
2025-07-27 17:38:40 +0300

    Небольшие изменения в структуре файлов

---
+++ myapi/shop_views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework import viewsets
+
+from serializers import *
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class AuthorsViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all().distinct()
+    serializer_class = AuthorSerializer
+
+class GenresViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all().distinct()
+    serializer_class = GenreSerializer
+
+class YearViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Book.objects.values('year').distinct().order_by("-year")
+    serializer_class = YearSerializer
+
+class GenreBookListAPIView(generics.ListAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        genre_slug = self.kwargs['slug']
+
+        genre = get_object_or_404(Genre, slug=genre_slug)
+        return Book.objects.filter(genres=genre)
+
+class AuthorBookListAPIView(generics.ListAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        author_slug = self.kwargs['slug']
+
+        author = get_object_or_404(Author, slug=author_slug)
+        return Book.objects.filter(authors=author)
+
+class YearBookListAPIView(generics.ListAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        year = self.kwargs['year']
+
+        return Book.objects.filter(year=year)
+
+class SearchBookListAPIView(generics.ListAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = Book.objects.all()
+
+        search_query = self.request.GET.get('q', '')
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(authors__name__icontains=search_query)
+            ).distinct()
+
+        return queryset
--- myapi/urls.py
+++ myapi/urls.py
@@ -1,25 +1,26 @@
 from django.urls import include, path
 from rest_framework import routers
-from . import views
+from . import auth_views
+from . import shop_views
 
 router = routers.DefaultRouter()
-router.register(r'books', views.BookViewSet, basename='book')
-router.register(r'authors', views.AuthorsViewSet)
-router.register(r'genres', views.GenresViewSet)
-router.register(r'years', views.YearViewSet, basename='year')
+router.register(r'books', shop_views.BookViewSet, basename='book')
+router.register(r'authors', shop_views.AuthorsViewSet)
+router.register(r'genres', shop_views.GenresViewSet)
+router.register(r'years', shop_views.YearViewSet, basename='year')
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('genre/<slug:slug>/', views.GenreBookListAPIView.as_view(), name='api-genre'),
-    path('author/<slug:slug>/', views.AuthorBookListAPIView.as_view(), name='api-author'),
-    path('year/<int:year>/', views.YearBookListAPIView.as_view(), name='api-year'),
-    path('search/', views.SearchBookListAPIView.as_view(), name='api-search'),
+    path('genre/<slug:slug>/', shop_views.GenreBookListAPIView.as_view(), name='api-genre'),
+    path('author/<slug:slug>/', shop_views.AuthorBookListAPIView.as_view(), name='api-author'),
+    path('year/<int:year>/', shop_views.YearBookListAPIView.as_view(), name='api-year'),
+    path('search/', shop_views.SearchBookListAPIView.as_view(), name='api-search'),
 
-    path('register/', views.RegistrationAPIView.as_view(), name='register'),
-    path('login/', views.LoginAPIView.as_view(), name='login'),
-    path('logout/', views.LogoutAPIView.as_view(), name='logout'),
-    path('auth-status/', views.AuthStatuAPIView.as_view(), name='hello'),
+    path('register/', auth_views.RegistrationAPIView.as_view(), name='register'),
+    path('login/', auth_views.LoginAPIView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutAPIView.as_view(), name='logout'),
+    path('auth-status/', auth_views.AuthStatuAPIView.as_view(), name='hello'),
 
     path('api-auth/', include('rest_framework.urls',  namespace='rest_fram'))
 ]
